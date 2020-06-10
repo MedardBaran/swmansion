95bc5b488bafae6d26271a5e1696241932ba8a2e
Medard <medzio@gmail.com>
2020-06-10 16:57:36 +0200

    my version

--- task1.py
+++ task1.py
@@ -4,9 +4,51 @@ def short_function(a, b):
     return [i for i in a if not isprime(Counter(b)[i])]
 
 
+def _is_prime(n) -> bool:
+    """also available here: https://en.wikipedia.org/wiki/Primality_test#Pseudocode"""
+
+    if n <= 3:
+        return n > 1
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+
+def my_function(a, b):
+    excluded = set()
+    primes = set()
+
+    result = []
+
+    for n in a:
+        if n in excluded:
+            continue
+        elif n in result:
+            result.append(n)
+        else:
+            i = b.count(n)
+            if i in primes:
+                excluded.add(n)
+            if _is_prime(i):
+                primes.add(i)
+                excluded.add(n)
+            else:
+                result.append(n)
+
+    return result
+
+
 if __name__ == '__main__':
     a = [2, 3, 9, 2, 5, 1, 3, 7, 10]
     b = [2, 1, 3, 4, 3, 10, 6, 6, 1, 7, 10, 10, 10]
     expected_c = [2, 9, 2, 5, 7, 10]
 
     assert short_function(a, b) == expected_c
+    assert my_function(a, b) == expected_c
